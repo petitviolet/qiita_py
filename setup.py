42a77596e1b17dc32e2f3c1f2991bf797ef5b6e6
petitviolet <violethero0820@gmail.com>
2014-12-20 17:26:43 +0900

    init setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+''' Wrapper for Qiita API v2
+
+created by @petitviolet
+http://petitviolet.mit-license.org/
+'''
+
+import os
+from setuptools import setup, find_packages
+from qiita_v2 import (__author__, __license__, __version__, __name__)
+
+description = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+setup(
+    name=__name__,
+    author=__author__,
+    version=__version__,
+    license=__license__,
+    author_email='violethero0820@gmail.com',
+    url='http://github.com/petitviolet/qiita_py',
+    description='Python Wrapper for Qiita API v2',
+    long_description=description,
+    platforms='any',
+    packages=find_packages(),
+    install_requires=['pyyaml', 'requests'],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+)
